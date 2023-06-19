82ff64a30a7e65bb8137d394ffd1bc01111a1fbf
dydtmd6801 <dydtmd6801@naver.com>
2023-06-19 09:43:27 +0900

    fix: 데이터베이스, ID, PW 변경 및 Bean 애노테이션 추가

--- src/main/java/config/DBConfig.java
+++ src/main/java/config/DBConfig.java
@@ -1,17 +1,19 @@
 package config;
 
 import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DBConfig {
 
+    @Bean
     public DataSource dataSource() {
         DataSource ds = new DataSource();
         ds.setDriverClassName("com.mysql.jdbc.Driver");
-        ds.setUrl("jdbc:mysql://localhost/news?allowPublicKeyRetrieval=true&useSSL=false&characterEncoding=utf8");
-        ds.setUsername("news");
-        ds.setPassword("news");
+        ds.setUrl("jdbc:mysql://localhost/project01?allowPublicKeyRetrieval=true&useSSL=false&characterEncoding=utf8");
+        ds.setUsername("project01");
+        ds.setPassword("project01");
         ds.setInitialSize(2);
         ds.setMaxActive(100);
         ds.setTestWhileIdle(true);
