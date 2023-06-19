7010da3ae77c36b2d048a5b2c68e0ab53cfd0763
dydtmd6801 <dydtmd6801@naver.com>
2023-06-19 15:13:04 +0900

    feat: 회원가입 입력값 DB에 저장

---
+++ src/main/java/dao/MemberDao.java
@@ -0,0 +1,42 @@
+package dao;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import regist.RegistDto;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+
+public class MemberDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public MemberDao(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public void insert(RegistDto registDto) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(
+                new PreparedStatementCreator() {
+                    @Override
+                    public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+                        PreparedStatement ps = con.prepareStatement("insert into MEMBER (`USERID`, `PASSWORD`, `NAME`, `PHONENUMBER`, `REGISTDATE`) values (?,?,?,?,?)", new String[]{"ID"});
+                        ps.setString(1, registDto.getUserId());
+                        ps.setString(2, registDto.getPassword());
+                        ps.setString(3, registDto.getName());
+                        ps.setString(4, registDto.getPhoneNumber());
+                        ps.setString(5, String.valueOf(LocalDateTime.now()));
+                        return ps;
+                    }
+                }
+        );
+        Number keyValue = keyHolder.getKey();
+        registDto.setId(keyValue.longValue());
+    }
+}
