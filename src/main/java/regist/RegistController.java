811f1e8ea18abb9c278edad7c84b4617476bb692
dydtmd6801 <dydtmd6801@naver.com>
2023-06-20 14:43:40 +0900

    feat: 입력한 비밀번호와 비밀번호 확인 검증 추가

--- src/main/java/regist/RegistController.java
+++ src/main/java/regist/RegistController.java
@@ -1,7 +1,6 @@
 package regist;
 
 import exception.DuplicateMemberException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -45,6 +44,9 @@ public class RegistController {
     @PostMapping("/step3")
     public String step3(RegistDto registDto, Model model) {
         try {
+            if(!registDto.getPassword().equals(registDto.getConfirmPassword())){
+                return "/regist/step2";
+            }
             registService.join(registDto);
             model.addAttribute("registDto", registDto);
             return "/regist/step3";
