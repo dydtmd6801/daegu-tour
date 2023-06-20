4678cb18bb8911574a535169451a7393bab3f685
dydtmd6801 <dydtmd6801@naver.com>
2023-06-20 14:08:53 +0900

    test: join에 실패할 경우, 예외처리 테스트

--- src/test/java/RegistServiceTest.java
+++ src/test/java/RegistServiceTest.java
@@ -1,12 +1,13 @@
 import config.DBConfig;
 import dao.MemberDao;
+import exception.DuplicateMemberException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import regist.RegistDto;
 import regist.RegistService;
 
-import java.lang.reflect.Member;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class RegistServiceTest {
 
@@ -27,6 +28,19 @@ public class RegistServiceTest {
         newMember.setName("운영자");
         newMember.setPhoneNumber("010-1122-3344");
         registService.join(newMember);
+    }
+
+    @Test
+    public void failJoinTest() {
+        joinTest();
+
+        RegistDto newMember = new RegistDto();
+        newMember.setUserId("admin");
+        newMember.setPassword("admin");
+        newMember.setName("운영자");
+        newMember.setPhoneNumber("010-1122-3344");
+
+        assertThatThrownBy(() -> registService.join(newMember)).isInstanceOf(DuplicateMemberException.class);
     }
 
 }
