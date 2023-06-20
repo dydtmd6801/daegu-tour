cc487e29814eb5d17ec428d1d4575cc7357b241b
dydtmd6801 <dydtmd6801@naver.com>
2023-06-20 16:29:44 +0900

    feat: 에러 메시지를 출력하기 위해 메시지 번들 사용

--- src/main/java/config/MvcConfig.java
+++ src/main/java/config/MvcConfig.java
@@ -1,6 +1,8 @@
 package config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
@@ -18,5 +20,12 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         registry.jsp("/WEB-INF/view/",".jsp");
+    }
+
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource ms = new ResourceBundleMessageSource();
+        ms.setBasename("message.label");
+        ms.setDefaultEncoding("UTF-8");
+        return ms;
     }
 }
