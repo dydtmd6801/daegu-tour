f3e1216e4fb37e60356b5acc2a273434108b405e
dydtmd6801 <dydtmd6801@naver.com>
2023-06-22 16:34:35 +0900

    feat: 회원가입 시, 사용자 입력값 규칙 추가

--- src/main/java/regist/RegistValidator.java
+++ src/main/java/regist/RegistValidator.java
@@ -4,7 +4,23 @@ import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegistValidator implements Validator {
+
+    private static final String userIdRegExp = "^[a-zA-Z0-9]*$";
+    private static final String passwordRegExp = "^(?=.*?[A-Za-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$";
+    private static final String phoneNumberRegExp = "^[0-9]{2,3}-[0-9]{3,4}-[0-9]{4}$";
+    private Pattern userIdPattern;
+    private Pattern passwordPattern;
+    private Pattern phoneNumberPattern;
+
+    public RegistValidator() {
+        userIdPattern = Pattern.compile(userIdRegExp);
+        passwordPattern = Pattern.compile(passwordRegExp);
+        phoneNumberPattern = Pattern.compile(phoneNumberRegExp);
+    }
 
     @Override
     public boolean supports(Class<?> clazz) {
@@ -13,10 +29,33 @@ public class RegistValidator implements Validator {
 
     @Override
     public void validate(Object target, Errors errors) {
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "userId","required");
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password","required");
+        Matcher matcher;
+        RegistDto registDto = (RegistDto) target;
+        if(registDto.getUserId() == null || registDto.getUserId().trim().isEmpty()) {
+            errors.rejectValue("userId","required");
+        } else {
+            matcher = userIdPattern.matcher(registDto.getUserId());
+            if(!matcher.matches()) {
+                errors.rejectValue("userId","bad");
+            }
+        }
+        if(registDto.getPassword() == null || registDto.getPassword().trim().isEmpty()) {
+            errors.rejectValue("password","required");
+        } else {
+            matcher = passwordPattern.matcher(registDto.getPassword());
+            if(!matcher.matches()) {
+                errors.rejectValue("password","bad");
+            }
+        }
+        if(registDto.getPhoneNumber() == null || registDto.getPhoneNumber().trim().isEmpty()) {
+            errors.rejectValue("phoneNumber","required");
+        } else {
+            matcher = phoneNumberPattern.matcher(registDto.getPhoneNumber());
+            if(!matcher.matches()) {
+                errors.rejectValue("phoneNumber","bad");
+            }
+        }
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "confirmPassword","required");
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name","required");
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "phoneNumber","required");
     }
 }
