e4fb15c04b229461be09edfae6064722217b817a
dydtmd6801 <dydtmd6801@naver.com>
2023-06-22 17:48:10 +0900

    test: 사용자를 찾을 수 없을 경우, 로그인 실패 테스트
    사용자가 입력한 ID 값으로 DB에 조회했을때, 값이 없는 경우 예외를 발생

--- src/test/java/LoginServiceTest.java
+++ src/test/java/LoginServiceTest.java
@@ -1,13 +1,16 @@
 import config.DBConfig;
 import dao.MemberDao;
+import exception.MemberNotFoundException;
 import login.LoginDto;
 import login.LoginService;
+import org.assertj.core.api.ThrowableAssert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import regist.RegistDto;
 import regist.RegistService;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LoginServiceTest {
 
@@ -28,5 +31,14 @@ public class LoginServiceTest {
         RegistDto memberInfo = loginService.Login(loginInfo);
 
         assertThat(loginInfo.getUserId()).isEqualTo(memberInfo.getUserId());
+    }
+
+    @Test
+    public void loginFailTest1() {
+        LoginDto loginInfo = new LoginDto();
+        loginInfo.setUserId("aabbcc");
+        loginInfo.setUserPassword("aabbcc");
+
+        assertThatThrownBy(() -> loginService.Login(loginInfo)).isInstanceOf(MemberNotFoundException.class);
     }
 }
