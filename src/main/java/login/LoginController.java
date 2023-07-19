37e7b002d64049b2979aead52d685a28d69df77a
dydtmd6801 <dydtmd6801@naver.com>
2023-07-19 17:51:33 +0900

    feat: 네아로(네이버 아이디 로그인) 컨트롤러 추가

--- src/main/java/login/LoginController.java
+++ src/main/java/login/LoginController.java
@@ -1,15 +1,17 @@
 package login;
 
+import com.github.scribejava.core.model.OAuth2AccessToken;
 import exception.MemberNotFoundException;
 import exception.NotMatchPasswordException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import regist.RegistDto;
 
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 @Controller
 @RequestMapping("/login")
@@ -17,20 +19,32 @@ public class LoginController {
 
     private LoginService loginService;
 
+    private NaverLoginBO naverLoginBO;
+    private String apiResult = null;
+
     public void setLoginService(LoginService loginService) {
         this.loginService = loginService;
     }
 
+    @Autowired
+    private void setNaverLoginBO(NaverLoginBO naverLoginBO) {
+        this.naverLoginBO = naverLoginBO;
+    }
+
     @GetMapping
-    public String showLogin(LoginDto loginDto, HttpSession session) {
+    public String showLogin(LoginDto loginDto, HttpSession session, Model model) {
         if(session.getAttribute("AuthInfo") == null) {
+            String naverAuthUrl = naverLoginBO.getAuthorizationUrl(session);
+            model.addAttribute("url", naverAuthUrl);
             return "login/login";
         }
         return "../../index";
     }
 
     @PostMapping
-    public String login(LoginDto loginDto, Errors errors, HttpSession session) {
+    public String login(LoginDto loginDto, Errors errors, HttpSession session, Model model) {
+        String naverAuthUrl = naverLoginBO.getAuthorizationUrl(session);
+        model.addAttribute("url", naverAuthUrl);
         new LoginValidator().validate(loginDto, errors);
         if(errors.hasErrors()) {
             return "login/login";
@@ -49,4 +63,16 @@ public class LoginController {
         }
     }
 
+    @RequestMapping(value="/callback", method = { RequestMethod.GET, RequestMethod.POST })
+    public String callback(Model model, @RequestParam String code, @RequestParam String state, HttpSession session) throws IOException {
+        OAuth2AccessToken oauthToken;
+
+        oauthToken = naverLoginBO.getAccessToken(session, code, state);
+
+        apiResult = naverLoginBO.getUserProfile(oauthToken);
+
+        model.addAttribute("result", apiResult);
+
+        return "redirect:/index";
+    }
 }
