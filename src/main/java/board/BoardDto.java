b0ad16a7ed4a5cf0d98fb322aa34508762f91990
dydtmd6801 <dydtmd6801@naver.com>
2023-07-21 16:32:04 +0900

    feat: 게시글에 들어갈 요소 Dto로 생성

---
+++ src/main/java/board/BoardDto.java
@@ -0,0 +1,50 @@
+package board;
+
+public class BoardDto {
+
+    private long id;
+    private String title;
+    private String writer;
+    private String content;
+    private String date;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
