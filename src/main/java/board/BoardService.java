38b8b691563555a40ed2ddeb29297e373b2bb525
dydtmd6801 <dydtmd6801@naver.com>
2023-08-01 15:20:25 +0900

    feat: 전체 게시글 목록 조회하는 서비스

--- src/main/java/board/BoardService.java
+++ src/main/java/board/BoardService.java
@@ -1,5 +1,7 @@
 package board;
 
+import java.util.List;
+
 public class BoardService {
 
     private BoardDao boardDao;
@@ -10,5 +12,10 @@ public class BoardService {
 
     public void write(BoardDto boardDto) throws NullPointerException {
         boardDao.insert(boardDto);
+    }
+
+    public List<BoardDto> listAll() {
+        List<BoardDto> results = boardDao.searchAll();
+        return results;
     }
 }
