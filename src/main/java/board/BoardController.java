dc8c1c93fe195b201e936b46dec67e11e315e66a
dydtmd6801 <dydtmd6801@naver.com>
2023-08-02 13:17:40 +0900

    feat: 게시판 컨트롤러 생성

---
+++ src/main/java/board/BoardController.java
@@ -0,0 +1,26 @@
+package board;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+
+    private BoardService boardService;
+
+    public void setBoardService(BoardService boardService) {
+        this.boardService = boardService;
+    }
+
+    @GetMapping
+    public String board(Model model) {
+        List<BoardDto> boards = boardService.listAll();
+        model.addAttribute("boards", boards);
+        return "/board/list";
+    }
+}
