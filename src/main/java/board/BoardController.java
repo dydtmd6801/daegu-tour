687aec9cc188ae13e2b6de0cb641c04ed6215676
dydtmd6801 <dydtmd6801@naver.com>
2023-08-04 10:24:30 +0900

    feat: 게시글 내용 보는 경로 생성

--- src/main/java/board/BoardController.java
+++ src/main/java/board/BoardController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -23,5 +24,12 @@ public class BoardController {
         List<BoardDto> boards = boardService.listAll();
         model.addAttribute("boards", boards);
         return "/board/list";
+    }
+
+    @GetMapping("/detail")
+    public String showDetail(@RequestParam long boardId, Model model) {
+        BoardDto boardDetail = boardService.searchDetail(boardId);
+        model.addAttribute("boardDetail", boardDetail);
+        return "/board/detail";
     }
 }
