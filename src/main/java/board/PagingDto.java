0ea8195c67f49bae46656853934b85691a1e8b17
dydtmd6801 <dydtmd6801@naver.com>
2023-09-01 16:00:59 +0900

    feat: 페이징 기법에 사용되는 값 생성

---
+++ src/main/java/board/PagingDto.java
@@ -0,0 +1,47 @@
+package board;
+
+public class PagingDto {
+
+    private static final int SHOW_BOARD_NUMBER = 10;
+
+    private int startPage;
+    private int endPage;
+    private int nowPage;
+    private int totalPage;
+
+    public int getShowBoardNumber() {
+        return SHOW_BOARD_NUMBER;
+    }
+
+    public int getStartPage() {
+        return startPage;
+    }
+
+    public void setStartPage(int startPage) {
+        this.startPage = startPage;
+    }
+
+    public int getEndPage() {
+        return endPage;
+    }
+
+    public void setEndPage(int endPage) {
+        this.endPage = endPage;
+    }
+
+    public int getNowPage() {
+        return nowPage;
+    }
+
+    public void setNowPage(int nowPage) {
+        this.nowPage = nowPage;
+    }
+
+    public int getTotalPage() {
+        return totalPage;
+    }
+
+    public void setTotalPage(int totalPage) {
+        this.totalPage = totalPage;
+    }
+}
