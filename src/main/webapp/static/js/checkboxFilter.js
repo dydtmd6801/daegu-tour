83358f77f5d394af2030a7ca4b55bcfe38ca8725
dydtmd6801 <dydtmd6801@naver.com>
2023-09-04 16:08:33 +0900

    feat: 필터 기능 수정 및 함수 분할

--- src/main/webapp/static/js/checkboxFilter.js
+++ src/main/webapp/static/js/checkboxFilter.js
@@ -2,24 +2,29 @@ let filters = document.querySelectorAll(".filter");
 let gugun = document.querySelectorAll(".gugun");
 let filterStr = "";
 
+const deSelect = () => {
+    for(let i = 0; i < filters.length; i++) {
+        if(filters[i].checked) {
+            filterStr += filters[i].id + ".";
+            gugun[i].style.backgroundColor="#6c757d";
+            gugun[i].style.color="#ffffff";
+        } else {
+            gugun[i].style.removeProperty("color");
+            gugun[i].style.removeProperty("background-color");
+        }
+    }
+}
+
 filters.forEach(filter => {
     filter.addEventListener("change", () => {
-        for(let i = 0; i < filters.length; i++) {
-            if(filters[i].checked) {
-                filterStr += filters[i].id + ".";
-                gugun[i].style.backgroundColor="#6c757d";
-                gugun[i].style.color="#ffffff";
-            } else {
-                gugun[i].style.removeProperty("color");
-                gugun[i].style.removeProperty("background-color");
-            }
-        }
+        deSelect();
         $.ajax({
             url: "/tour/tourFilter",
             data: {"filter": filterStr},
             type: "GET",
             contentType: "application/x-www-form-urlencoded; charset=UTF-8",
             success: function (data) {
+                document.querySelector("#searchBar").value = "";
                 $('#tourList').html(data);
             },
             error: function () {
