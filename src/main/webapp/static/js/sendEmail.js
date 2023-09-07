6a9869e318166fdc8c4d0c49db64e2d343c9cc42
dydtmd6801 <dydtmd6801@naver.com>
2023-09-07 15:05:15 +0900

    feat: 이메일 인증 번호 확인 기능 추가 및 ajax 기능 수정

--- src/main/webapp/static/js/sendEmail.js
+++ src/main/webapp/static/js/sendEmail.js
@@ -1,18 +1,34 @@
 let code;
+let authFlag = 0;
 
 $("#checkMailBtn").click(function () {
     const email = $("#email").val() + "@" + $("#email2").val();
-    const authPassword = $("inputAuthPassword");
 
     $.ajax({
         url: "/regist/checkMailAuth",
         data: {"email": email},
-        type: "get",
+        type: "GET",
         success: function(data) {
-            authPassword.removeAttr("display");
+            $("#inputAuthPassword").removeAttr("style");
             code = data;
             alert("인증번호가 전송되었습니다.");
+        },
+        error: function(request, status, error) {
+            alert("code : " + request.status + "\n message : " + request.responseText + "\n error : " + error);
         }
     })
 })
 
+const authResult = $("#authResult")
+
+$("#checkAuth").click(function () {
+    if ($("#authPassword").val() === code) {
+        authResult.html("인증이 완료되었습니다.");
+        authResult.css("color", "#198754");
+        authFlag = 1;
+        return;
+    }
+    authResult.html("인증번호가 올바르지 않습니다.");
+    authResult.css("color","#dc3545");
+    authFlag = 0;
+})
