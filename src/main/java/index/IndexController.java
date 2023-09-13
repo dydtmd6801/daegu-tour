dec859bcf344c7559ebfb23e039ca0a47f7f0898
dydtmd6801 <dydtmd6801@naver.com>
2023-09-13 14:43:52 +0900

    feat: 로그인 세션 여부에 따라 페이지 접근 차단 기능 생성

--- src/main/java/index/IndexController.java
+++ src/main/java/index/IndexController.java
@@ -1,9 +1,11 @@
 package index;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/index")
@@ -11,7 +13,8 @@ public class IndexController {
 
     @GetMapping
     @PostMapping
-    public String index() {
+    public String index(@RequestParam(required = false) String checkSession, Model model) {
+        model.addAttribute("checkSession", checkSession);
         return "../../index";
     }
 }
