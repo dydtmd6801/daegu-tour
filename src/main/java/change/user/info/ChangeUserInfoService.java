cb63a787e4b4e0c91bc6fffb454f9a4e6a0afe79
dydtmd6801 <dydtmd6801@naver.com>
2023-09-13 14:37:29 +0900

    feat: 비밀번호로 인증 및 개인정보 출력 비즈니스 로직 생성

---
+++ src/main/java/change/user/info/ChangeUserInfoService.java
@@ -0,0 +1,21 @@
+package change.user.info;
+
+import regist.MemberDao;
+import regist.RegistDto;
+
+public class ChangeUserInfoService {
+
+    private MemberDao memberDao;
+
+    public ChangeUserInfoService(MemberDao memberDao) {
+        this.memberDao = memberDao;
+    }
+
+    public RegistDto authByPassword(String id, String password) {
+        RegistDto memberInfo = memberDao.findById(id);
+        if (!memberInfo.getPassword().equals(password)) {
+            return null;
+        }
+        return memberInfo;
+    }
+}
