e3f58e1d143b671b1d061705d9282900d96baf7d
dydtmd6801 <dydtmd6801@naver.com>
2023-09-13 09:15:45 +0900

    feat: 정보 변경에 필요한 입력값 getter/setter 생성

---
+++ src/main/java/change/user/info/ChangeUserInfoDto.java
@@ -0,0 +1,50 @@
+package change.user.info;
+
+public class ChangeUserInfoDto {
+
+    private String userId;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String password;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
